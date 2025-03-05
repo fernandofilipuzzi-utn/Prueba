0429f1168ff77afa62726ebef989a6af2605a371
fernando rafael filipuzzi <fernandofilipuzzi.ng@gmail.com>
2025-03-05 10:14:12 -0300

    sumando la clase persona

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+// See https://aka.ms/new-console-template for more information
+using Prueba;
+
 Console.WriteLine("Hello, World!");
+
+
+Persona persona = new Persona("Juan", 30);
+
+Console.WriteLine(persona);
